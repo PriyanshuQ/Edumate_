0551ef48fa6b47de9e46da1cec750ffb09883ca6
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-04 03:12:15 +0530

    commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function Login() {
+
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/login", values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to home page");
+        localStorage.setItem("token", response.data.data);
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <div className="authentication">
       <div className="authentication-from card p-3">
         <h1 className="card-title">Welcome Back</h1>
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={onFinish}>
           <Form.Item label="Email" name="email">
             <Input placeholder="Email" />
           </Form.Item>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function Register() {
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/register", values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        navigate("/login");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <div className="authentication">
       <div className="authentication-from card p-3">
         <h1 className="card-title">Hey there!</h1>
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={onFinish}>
           <Form.Item label="Name" name="name">
             <Input placeholder="Name" />
           </Form.Item>
