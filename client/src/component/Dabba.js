220c8255c9564773169967566280a7415e0d1345
Chirag Ailani <chiragailani35@gmail.com>
2023-06-03 17:26:07 +0530

    more bugs solved

--- client/src/component/Dabba.js
+++ client/src/component/Dabba.js
@@ -6,39 +6,52 @@ import {
   CardContent,
   Button,
   Typography,
+  styled,
 } from "@mui/material";
-import styled from "@emotion/styled";
 
 const Boxstyle = styled(Card)`
-  width: 300px;
+  width: 315px;
+  height: 225px;
   background: #ffffff;
   margin: auto;
   margin-bottom: 10px;
+  objectfit: "fill";
+`;
+
+const CT = styled(Box)`
+  height: 100px;
+`;
+
+const CTH = styled(Typography)`
+  width: 100%;
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 5px;
 `
 
-const Dabba = ({data}) => {
+const CD = styled(Typography)`
+  font-size: 14px;
+`;
+
+const Dabba = ({ data }) => {
   return (
     <>
       <Boxstyle variant="outlined">
         <CardContent>
-          <Typography>
-            {data.name}
-          </Typography>
-          <Typography >
-          {data.code}
-          </Typography>
-          <Typography>
-          {data.credit}
-          </Typography>
+          <CT>
+            <CTH>Course Title : {data.name}</CTH>
+          </CT>
+          <Box>
+            <CD>Course Code : {data.code}</CD>
+            <CD>Course Credit : {data.credit}</CD>
+          </Box>
         </CardContent>
         <CardActions>
           <Button size="small">Download</Button>
         </CardActions>
       </Boxstyle>
     </>
-    
   );
 };
-
 
 export default Dabba;
