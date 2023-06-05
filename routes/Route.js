43d9420abf0c6bd0a62abfc17deebd0200c205b1
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-06 04:13:29 +0530

    commit

--- routes/Route.js
+++ routes/Route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const courseController = require("../controllers/course-controller");
 const router = express.Router();
 
-const userCntroller = require("../controllers/user-controller")
+const userCntroller = require("../controllers/user-controller");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // add/get courses
 router.get("/getcourses", courseController.getAllCourses);
@@ -11,6 +12,8 @@ router.post("/addcourse", courseController.addCourse);
 // register/login user
 router.post("/register",userCntroller.register);
 router.post("/login",userCntroller.login);
+
+router.post("/get-user-info-by-id", authMiddleware, userCntroller.info)
 
 
 module.exports = router;
